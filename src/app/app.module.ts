03c901d5e361afb50070f646f8075035737502db
Mario <jorge.fernandez@pragma.com.co>
2020-06-16 23:04:43 -0500

    integration with Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from '../pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ROUTES } from './routes';
+import { HomeComponent } from '../pages/home/home.component';
 
-export const appRouter: Routes = [];
+export const appRouter: Routes = [
+    {
+        path: ROUTES.HOME,
+        component: HomeComponent,
+    },
+    {
+      path: '',
+      redirectTo: ROUTES.HOME,
+      pathMatch: 'full',
+    },
+    {
+        path: '**',
+        redirectTo: ROUTES.HOME,
+    },
+];
