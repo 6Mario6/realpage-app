205a522c29374c134980f36e340b88fdf0f585a6
Mario <jorge.fernandez@pragma.com.co>
2020-06-17 10:13:29 -0500

    components to create user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HomeComponent } from '../pages/home/home.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { ListUsersComponent } from '../components/list-users/list-users.component';
+import { UserComponent } from '../components/user/user.component';
+import { UserFormComponent } from '../components/user-form/user-form.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    ListUsersComponent,
+    UserComponent,
+    UserFormComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
